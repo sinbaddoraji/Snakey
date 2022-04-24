05ce767d790f98da3ca33a1575be9c1ed212d21b
Osinachi Nwagboso Samuel <osinachinwagboso@gmail.com>
2022-04-24 16:04:29 +0100

    Add project files.

---
+++ Snakey/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+string gameArena;
+
+void Init()
+{
+
+    gameArena = "";
+    Console.CursorSize = 50;
+    
+    for (int y = 0; y < Console.WindowHeight - 5; y++)
+    {
+        for (int x = 1; x < Console.WindowWidth; x++)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.White;
+            if (x == 1 || x == Console.WindowWidth - 1)
+            {
+                Console.Write("|");
+            }
+            else if (y == 0 || y == Console.WindowHeight - 6)
+            {
+                Console.Write("-");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+Init();
+Console.WriteLine(gameArena);
+var _snake = new Snakey.Snake(5, 5);
+
+int _direction = 1;
+int _frame_rate = 200;
+
+//-1 = No movement, 0 - Left, 1 - Right, 2 - Up, 3 - Down
+ConsoleKey[] directions = new ConsoleKey[] { ConsoleKey.A, ConsoleKey.D, ConsoleKey.W, ConsoleKey.S };
+while (true)
+{
+    if(Console.KeyAvailable)
+    {
+        ConsoleKey choice = Console.ReadKey(true).Key;
+        
+
+        if(choice == ConsoleKey.R)
+        {
+            _snake.GrowSnake(_direction);
+        }
+        else
+        {
+            _direction = Array.IndexOf(directions, choice);
+        }
+    }
+    _snake.MoveSnake(_direction);
+    Thread.Sleep(_frame_rate);
+}
+
+
---
+++ Snakey/Snake.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakey
+{
+    internal class Snake
+    {
+        List<SnakeObject> head = new List<SnakeObject>();
+        //-1 = No movement, 0 - Left, 1 - Right, 2 - Up, 3 - Down
+        public Snake(int x, int y)
+        {
+            head.Add(new SnakeObject(x, y));
+        }
+
+        public void MoveSnake(int direction)
+        {
+            for(int i = head.Count - 1; i > 1; i--)
+            {
+                head[i] = head[i - 1];
+            }
+            head.Last().Move(direction);
+        }
+
+        public void GrowSnake(int direction)
+        {
+            SnakeObject newHead = new SnakeObject(head.Last().x, head.Last().y);
+            newHead.Move(direction);
+            head.Add(newHead);
+
+        }
+    }
+
+    internal class SnakeObject
+    {
+        public int x;
+        public int y;
+
+
+        public void Display()
+        {
+            Console.CursorLeft = x;
+            Console.CursorTop = y;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("x");
+        }
+
+        public SnakeObject(int x, int y)
+        {
+            this.x = x; 
+            this.y = y;
+        }
+
+        public void Move(int direction)
+        {
+            //-1 = No movement, 0 - Left, 1 - Right, 2 - Up, 3 - Down
+            Console.CursorLeft = x;
+            Console.CursorTop = y;
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(" ");
+
+            if (direction == 0 && x > 1)
+            {
+                x--;
+            }
+            if (direction == 1 && x < Console.WindowWidth - 3)
+            {
+                x++;
+            }
+            if (direction == 2 && y > 1)
+            {
+                y--;
+            }
+            if (direction == 3 && y < Console.WindowHeight - 7)
+            {
+                y++;
+            }
+
+            Display();
+        }
+
+    }
+}
